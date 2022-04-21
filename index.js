d9a3583caba0ccd8ea52264f397d3230646f15c2
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2022-04-21 18:59:58 +1200

    opps

--- index.js
+++ index.js
@@ -21,7 +21,8 @@ router.get("/posts/:id", async (ctx) => {
 router.post("/posts", async (ctx) => {
   const query = await db("INSERT INTO posts (title, body) VALUES ($title, $body) RETURNING *", {
     title: ctx.body.value.get('title'),
-    body: ctx.body.value.get('body');
+    body: ctx.body.value.get('body')
+  });
   ctx.response.body = query;
 });
 

1aa346af470daa2f4f79f2dea0353d9e008c86d7
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2022-04-21 19:28:25 +1200

    env

--- db.js
+++ db.js
@@ -1,15 +1,10 @@
 import { Pool } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
-const pool = new Pool({
-  user: Deno.env.get("PGUSER") || '',
-  password: Deno.env.get("PGPASSWORD") || '',
-  database: Deno.env.get("PGDATABASE") || '',
-  hostname: Deno.env.get("PGHOST") || '',
-  port: Deno.env.get("PGPORT") || '5432',
-}, 2);
+const pool = new Pool(Deno.env.get('PG_CONNECTION_STRING') || '', 10);
 
 export default async (query, params) => {
+  console.log(pool)
   const client = await pool.connect();
   try {
   return (await client.queryObject(query, params)).rows || [];
